43e34843148783d1d4fd600824901adab5e810ca
kemillysantos-lab <kemillysf23@gmail.com>
2026-02-27 08:54:49 -0300

    add initial adventure script with path choices

---
+++ atividade1.py
@@ -0,0 +1,8 @@
+print ("Seguir até a floresta")
+caminho = input("Qual caminho seguir? (esquerda/direita) ")
+if (caminho == "esquerda"):
+    print("Enfrentar um lobo selvagem!")
+
+print (" Seguir até as toras de madeira")
+print (" Cortar uma lenha")
+print (" Seguir até o acampamento")

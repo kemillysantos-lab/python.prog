7a67adfc77f62bea71a8fdcb0947d011522b112f
kemillysantos-lab <kemillysf23@gmail.com>
2026-03-06 09:43:17 -0300

    replace even/odd checker with average calculation script for three grades

--- atividade1.py
+++ atividade1.py
@@ -1,5 +1,8 @@
-x = int(input("Digite um número inteiro: "))
-if x % 2 == 0:
-    print(f"O número {x} é par.")
-else:
-    print(f"O número {x} é ímpar.")
+m1 = float (input("Digite a nota do primeiro bimestre: "))
+m2 = float (input("Digite a nota do segundo bimestre: "))
+m3 = float (input("Digite a nota do terceiro bimestre: "))
+media = (m1 + m2 + m3) / 3
+if media >= 7:
+    print(f'Parabéns! Você foi aprovado com média {media}.')
+else: 
+    print(f'Infelizmente, você foi reprovado com média {media}.')
